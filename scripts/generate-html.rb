81880d56668adde7b4374ea77f09b1a47a2e398b
Jari Bakken <jari.bakken@gmail.com>
2014-07-19 00:22:32 +0200

    Switch to column charts + cut 2010 which only has data up to february

--- scripts/generate-html.rb
+++ scripts/generate-html.rb
@@ -66,6 +66,8 @@ end
 actors = actor_counts_by_year.keys.sort
 chart_data = [["Year", *actors]]
 
+years.delete(2010) # incomplete year
+
 years.sort.each do |year|
   actor_values = actors.map { |a| actor_counts_by_year[a][year] }
   chart_data << [year.to_s, *actor_values]
